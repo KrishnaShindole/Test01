4aafe40fc6a1ac6b83f56a0e659c7555d786fb8f
Krushna <Krushna@DESKTOP-NKBK0D1>
2022-10-29 23:03:32 +0530

    First Demo GitHub

---
+++ src/Basic/Demo01.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Demo01
+{
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Good Morning");
+		
+		     M1();
+	}
+	
+	public static void M1()
+	{
+		System.out.println("Hi");	
+	}
+	
+}
